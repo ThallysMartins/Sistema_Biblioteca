04a6376a7acfed36cb7d41152d688407f1e9458a
Thallys Martins <devjoker95@gmail.com>
2026-07-23 19:24:42 -0300

    Feat: cria rota para atualizar infomações de livros já criados || Refactor: trata validações e erros na rota de POST

--- Program.cs
+++ Program.cs
@@ -15,14 +15,23 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/api/livros", (BibliotecaDbContext banco) =>
+/*app.MapGet("/api/livros", (BibliotecaDbContext banco) =>
 {
     // Busca a lista de livros que está no banco e responde com Sucesso (Status 200)
     return Results.Ok(banco.Livros.ToList());
-});
+});*/
 
 app.MapPost("/api/livros", (Livro novoLivro, BibliotecaDbContext banco) =>
 {
+    if (string.IsNullOrWhiteSpace(novoLivro.Titulo) || string.IsNullOrWhiteSpace(novoLivro.Autor) || novoLivro.Autor.Trim().ToLower() == "string" || novoLivro.Titulo.Trim().ToLower() == "string")
+    {
+        return Results.BadRequest(new { mensagem = "O titulo e o autor são obrigatórios!" });
+    }
+    if (novoLivro.AnoLancamento <= 0)
+    {
+        return Results.BadRequest(new { mensagem = "Digite um ano de lançamento válido!" });
+    }
+
     banco.Livros.Add(novoLivro);
     banco.SaveChanges();
 
@@ -69,13 +78,13 @@ app.MapGet("api/livros", (string? tipoSelecionado, string? textoDigitado, Biblio
             break;
 
         case "titulo":
-            var palavrasTitulo = textoDigitado.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            consulta = consulta.Where(l => palavrasTitulo.Any(p => l.Titulo.Contains(p, StringComparison.OrdinalIgnoreCase)));
+            var palavrasTitulo = textoDigitado.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            consulta = consulta.Where(l => palavrasTitulo.Any(p => l.Titulo.ToLower().Contains(p, StringComparison.OrdinalIgnoreCase)));
             break;
 
         case "autor":
-            var palavrasAutor = textoDigitado.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            consulta = consulta.Where(l => palavrasAutor.Any(p => l.Autor.Contains(p, StringComparison.OrdinalIgnoreCase)));
+            var palavrasAutor = textoDigitado.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            consulta = consulta.Where(l => palavrasAutor.Any(p => l.Autor.ToLower().Contains(p, StringComparison.OrdinalIgnoreCase)));
             break;
 
         case "ano":
@@ -87,6 +96,24 @@ app.MapGet("api/livros", (string? tipoSelecionado, string? textoDigitado, Biblio
     }
 
     return Results.Ok(consulta.ToList());
+});
+
+app.MapPut("api/livros/{id:int}", (int id, Livro livroAtualizado, BibliotecaDbContext dados) =>
+{
+    var livroExistente = dados.Livros.Find(id);
+
+    if (livroExistente == null)
+    {
+        return Results.NotFound(new { mensagem = "Livro não encontrado para atualização!" });
+    }
+
+    livroExistente.Titulo = livroAtualizado.Titulo;
+    livroExistente.Autor = livroAtualizado.Autor;
+    livroExistente.AnoLancamento = livroAtualizado.AnoLancamento;
+
+    dados.SaveChanges();
+
+    return Results.Ok(livroExistente);
 });
 
 app.Urls.Add("http://localhost:5000"); // 👈 Obriga o C# a usar essa porta
